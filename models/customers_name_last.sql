42b49a925dad27c16f92429ed9cb0bedb5ae47ca
manishbhasinba@gmail.com <manishbhasinba@gmail.com>
2026-08-31 01:28:44 +0000

    new lastname

---
+++ models/customers_name_last.sql
@@ -0,0 +1,10 @@
+-- ============================================================
+-- customers_view: a VIEW built on top of the customers seed
+-- Simple columns first, calculated columns last (SQLFluff ST06)
+-- ============================================================
+
+select
+    customer_id,
+    city,
+    first_name || ' ' || last_name as full_name
+from {{ ref('customers') }}
